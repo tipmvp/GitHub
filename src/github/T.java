14a413adf4066a4f057609e61a024c0486fa5947
ttp <527813949@qq.com>
2019-05-08 22:15:16 +0800

    first commit

---
+++ src/github/T.java
@@ -0,0 +1,12 @@
+package github;
+
+/**
+ * @author XRZ
+ * @date 2019/5/8
+ * @Description :
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println("conmjgdm");
+    }
+}
